ae9da31bb549e4a17102ca8173aa4242bd95a66a
ISMurzin <ISMurzin@mephi.ru>
2023-03-20 08:57:07 +0300

    add simple carousel

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,13 +3,19 @@ import MainView from "./main/MainView.vue"
 import InfoView from "./info/InfoView.vue"
 import FloodView from "./flood/FloodView.vue"
 import RecoverView from "./recover/RecoverView.vue"
+import VueCarousel from "@chenfengyuan/vue-carousel"
+const data = [MainView, InfoView, FloodView, RecoverView]
 </script>
 
 <template>
-  <MainView />
-  <InfoView />
-  <FloodView />
-  <RecoverView />
+  <vue-carousel
+    :data="data"
+    :controls="false"
+    :indicators="true"
+    :autoplay="false"
+    direction="up"
+    :slideOnSwipe="true"
+  />
 </template>
 
 <style scoped lang="scss"></style>
