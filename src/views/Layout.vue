778fd45b1bbb88c33cd14c7189eab2e8a329f9ce
preisy <preus_3@mail.ru>
2023-03-25 16:38:47 +0300

    settings edits

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -5,7 +5,7 @@ import FloodView from "./flood/FloodView.vue"
 import RecoverView from "./recover/RecoverView.vue"
 import NecessityView from "./necessity/NecessityView.vue"
 import DocumentView from "./document/DocumentView.vue"
-import { Splide, SplideSlide } from "@splidejs/vue-splide"
+import { Splide as SplideComp, SplideSlide } from "@splidejs/vue-splide"
 // Default theme
 import "@splidejs/vue-splide/css"
 
@@ -19,22 +19,23 @@ const data = [MainView, InfoView, FloodView, RecoverView, NecessityView, Documen
 </script>
 
 <template>
-  <!-- <vue-carousel
-    :data="data"
-    :controls="true"
-    :indicators="true"
-    :autoplay="false"
-    direction="up"
-    :slideOnSwipe="true"
-  /> -->
-  <Splide :options="{ direction: 'ttb', wheel: true, height: '100vh', speed: 800, arrows: false, wheelMinThreshold: 10, wheelSleep: 1000 }">
+  <SplideComp :options="{
+    direction: 'ttb',
+    wheel: true,
+    height: '100vh',
+    autoHeight: true,
+    speed: 800,
+    arrows: false,
+    wheelMinThreshold: 10,
+    wheelSleep: 300,
+  }">
     <SplideSlide><MainView /></SplideSlide>
     <SplideSlide><InfoView /></SplideSlide>
     <SplideSlide><FloodView /></SplideSlide>
     <SplideSlide><RecoverView /></SplideSlide>
     <SplideSlide><NecessityView /></SplideSlide>
     <SplideSlide><DocumentView /></SplideSlide>
-  </Splide>
+  </SplideComp>
   <!-- <component v-for="d in data" :is="d" /> -->
 </template>
 
